13150bed97d5ece84af5c4d1eea885bbcb8a42e2
Lautaro Lara <lautaronicolas.lara@est.fi.uncoma.edu.ar>
2025-11-09 18:51:41 -0300

    a

--- Vista/estructura/header.php
+++ Vista/estructura/header.php
@@ -15,9 +15,9 @@ if (session_status() === PHP_SESSION_NONE) {
 //TODO                <li> <a href="/PWD-TP-FINAL/Vista/cliente/compras.php" class="btn btn-outline-primary mb-3">Mis Compras</a> </li>
 //TODO }elseif($_SESSION['rol'] == "admin"){
 //TODO    $opciones = <li> <a href="/PWD-TP-FINAL/Vista/admin/usuarios.php" class="btn btn-dark w-100 mb-3">Gestionar Usuarios</a> </li>
-//TODO     <li> <a href="/PWD-TP-FINAL/Vista/admin/roles.php" class="btn btn-dark w-100 mb-3">Gestionar Roles</a> </li>
-//TODO     <li> <a href="/PWD-TP-FINAL/Vista/admin/menu.php" class="btn btn-dark w-100 mb-3">Gestionar Menú</a> </li>
-//TODO     <li> <a href="/PWD-TP-FINAL/Vista/admin/productos.php" class="btn btn-dark w-100 mb-3">Gestionar Productos</a> </li>
+//TODO                <li> <a href="/PWD-TP-FINAL/Vista/admin/menu.php" class="btn btn-dark w-100 mb-3">Gestionar Menú</a> </li>
+//TODO                <li> <a href="/PWD-TP-FINAL/Vista/admin/roles.php" class="btn btn-dark w-100 mb-3">Gestionar Roles</a> </li>
+//TODO                <li> <a href="/PWD-TP-FINAL/Vista/admin/productos.php" class="btn btn-dark w-100 mb-3">Gestionar Productos</a> </li>
 //TODO }
 
 ?>
