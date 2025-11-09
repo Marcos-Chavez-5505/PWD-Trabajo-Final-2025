0232226bc4d13ce898007bebb4e55ce2a47d8172
Lautaro Lara <lautaronicolas.lara@est.fi.uncoma.edu.ar>
2025-11-09 18:50:52 -0300

    mas webadas

--- Vista/estructura/header.php
+++ Vista/estructura/header.php
@@ -1,7 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+// También para mostrar notices y warnings
+ini_set('error_reporting', E_ALL);
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+//TODO if ($_SESSION['rol'] == "cliente"){
+//TODO    $opciones = <li> <a href="/PWD-TP-FINAL/Vista/cliente/miCuenta.php" class="btn btn-outline-primary mb-3">Mi Cuenta</a> </li>
+//TODO                <li> <a href="/PWD-TP-FINAL/Vista/publica/productos.php" class="btn btn-outline-primary mb-3">Ver Productos</a> </li>
+//TODO                <li> <a href="/PWD-TP-FINAL/Vista/cliente/compras.php" class="btn btn-outline-primary mb-3">Mis Compras</a> </li>
+//TODO }elseif($_SESSION['rol'] == "admin"){
+//TODO    $opciones = <li> <a href="/PWD-TP-FINAL/Vista/admin/usuarios.php" class="btn btn-dark w-100 mb-3">Gestionar Usuarios</a> </li>
+//TODO     <li> <a href="/PWD-TP-FINAL/Vista/admin/roles.php" class="btn btn-dark w-100 mb-3">Gestionar Roles</a> </li>
+//TODO     <li> <a href="/PWD-TP-FINAL/Vista/admin/menu.php" class="btn btn-dark w-100 mb-3">Gestionar Menú</a> </li>
+//TODO     <li> <a href="/PWD-TP-FINAL/Vista/admin/productos.php" class="btn btn-dark w-100 mb-3">Gestionar Productos</a> </li>
+//TODO }
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -37,9 +55,8 @@ if (session_status() === PHP_SESSION_NONE) {
     </button>
     
     <ul class="dropdown-menu dropdown-menu-end">
+      <!-- ACÁ IRIA LA VARIABLE $opciones -->
         <li><a class="dropdown-item" href="#">Opción 1</a></li>
-        <li><a class="dropdown-item" href="#">Opción 2</a></li>
-        <li><a class="dropdown-item" href="#">Opción 3</a></li>
     </ul>
 </div>
 </nav>
