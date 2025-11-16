991694e767d6cfb248b1f82ccd92559422d58ada
LucasMartinez-Estudiante <lucas.martinez@est.fi.uncoma.edu.ar>
2025-11-16 19:41:31 -0300

    div de header que corta footer

--- Vista/estructura/header.php
+++ Vista/estructura/header.php
@@ -88,5 +88,4 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/PWD-TP-FINAL/configuracion.php";
   </div>
 </nav>
 
-<div class="container mt-4">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
